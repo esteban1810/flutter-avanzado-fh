913407f52191198e6f003bf68820f91e7ce7cd82
Esteban Sevilla <esteban@perspective.com.mx>
2022-04-07 13:28:19 -0500

    fix

---
+++ S12 Provider - Como manejador de estado/estados_app/lib/screens/screens_page1.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../models/usuario_model.dart';
+
+class Page1Screen extends StatelessWidget {
+   
+  const Page1Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page1Screen'),
+      ),
+      body: const _CustomBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.pushNamed(context, '/page2'),
+        child: const Icon(Icons.next_week_outlined),
+      ),
+    );
+  }
+}
+
+class _CustomBody extends StatelessWidget {
+  
+  const _CustomBody({
+    Key? key, 
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:  const [
+          Text('General',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold),),
+          Divider(),
+          ListTile(
+            title: Text('Nombre:'),
+          ),
+          ListTile(
+            title: Text('Edad:'),
+          ),
+          Text('Profesiones',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold),),
+          Divider(),
+          ListTile(
+            title: Text('Profesion 1'),
+          ),
+          ListTile(
+            title: Text('Profesion 1'),
+          ),
+          ListTile(
+            title: Text('Profesion 1'),
+          ),
+        ]),
+    );
+  }
+}
---
+++ estados_app/lib/screens/screens_page2.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Page2Screen extends StatelessWidget {
+   
+  const Page2Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page2Screen')
+      ),
+      body:  Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MaterialButton(
+            onPressed: (){
+            },
+            color: Colors.blue,
+            child: const Text(
+              'Establecer Usuario',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          MaterialButton(
+            onPressed: (){
+            },
+            color: Colors.blue,
+            child: const Text(
+              'Cambiar Edad',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          MaterialButton(
+            onPressed: (){},
+            color: Colors.blue,
+            child: const Text(
+              'Añadir Profesion',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+      ),
+    );
+  }
+}
