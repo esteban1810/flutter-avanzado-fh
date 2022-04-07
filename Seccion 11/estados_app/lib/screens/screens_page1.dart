9095e9e327f51b4e73fa19914e5b259c75d59134
Esteban Sevilla <esteban@perspective.com.mx>
2022-04-06 21:02:48 -0500

    125. Diseño de ambas pantallas

--- Seccion 11/estados_app/lib/screens/screens_page1.dart
+++ Seccion 11/estados_app/lib/screens/screens_page1.dart
@@ -10,13 +10,47 @@ class Page1Screen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Page1Screen'),
       ),
-      body: const Center(
-         child: Text('Page1Screen'),
-      ),
+      body: _CustomBody(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.pushNamed(context, '/page2'),
         child: const Icon(Icons.next_week_outlined),
       ),
+    );
+  }
+}
+
+class _CustomBody extends StatelessWidget {
+  const _CustomBody({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text('General',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold),),
+          Divider(),
+          ListTile(
+            title: Text('Nombre'),
+          ),
+          ListTile(
+            title: Text('Edad'),
+          ),
+          Text('Profesiones',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold),),
+          Divider(),
+          ListTile(
+            title: Text('Profesion 1'),
+          ),
+          ListTile(
+            title: Text('Profesion 1'),
+          ),
+          ListTile(
+            title: Text('Profesion 1'),
+          ),
+        ]),
     );
   }
 }
--- Seccion 11/estados_app/lib/screens/screens_page2.dart
+++ Seccion 11/estados_app/lib/screens/screens_page2.dart
@@ -10,8 +10,36 @@ class Page2Screen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Page2Screen'),
       ),
-      body: const Center(
-         child: Text('Page2Screen'),
+      body:  Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MaterialButton(
+            onPressed: (){},
+            color: Colors.blue,
+            child: const Text(
+              'Establecer Usuario',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          MaterialButton(
+            onPressed: (){},
+            color: Colors.blue,
+            child: const Text(
+              'Cambiar Edad',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          MaterialButton(
+            onPressed: (){},
+            color: Colors.blue,
+            child: const Text(
+              'Añadir Profesion',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
       ),
     );
   }
