6ae7929b09bc692e8d54ac3f6bc708b1cce347f3
Esteban Sevilla <esteban@perspective.com.mx>
2022-04-11 17:31:04 -0500

    147. Creando nuestro primer Cubit

---
+++ S13 Cubit/estados_app/lib/bloc/usuario/usuario_cubit.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+part 'usuario_state.dart';
+
+class UsuarioCubit extends Cubit<UsuarioState>{
+  UsuarioCubit() : super(UsuarioInitial());
+}
---
+++ S13 Cubit/estados_app/lib/bloc/usuario/usuario_state.dart
@@ -0,0 +1,7 @@
+part of 'usuario_cubit.dart';
+
+abstract class UsuarioState{}
+
+class UsuarioInitial extends UsuarioState{
+  final exists = false;
+}
